da3a7fea62b18b44d06b9f661ef2873cd17e445e
GlitcherOG <54088472+GlitcherOG@users.noreply.github.com>
2025-10-26 11:32:05 +1100

    func_00317890

--- src/bxrandom.cpp
+++ src/bxrandom.cpp
@@ -20,7 +20,18 @@ INCLUDE_ASM("bxrandom", AIrand);
 
 INCLUDE_ASM("bxrandom", AIrandf);
 
-INCLUDE_ASM("bxrandom", func_00317890);
+INCLUDE_ASM("bxrandom", func_00317890__Fff);
+#ifdef SKIP_ASM
+float AIrandf(float arg0, float arg1);
+
+float func_00317890(float param_1, float param_2)
+{
+	float fVar1;
+
+	fVar1 = AIrandf(-1.0, 1.0);
+	return param_1 + param_2 * fVar1;
+}
+#endif
 
 INCLUDE_ASM("bxrandom", func_003178E0);
 
